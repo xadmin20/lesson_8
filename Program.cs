6091db7be0c6dc206de380e40268c9c5cf8a9d4e
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-13 20:06:42 +0300

    Задача №56

--- Program.cs
+++ Program.cs
@@ -1,44 +1,44 @@
-﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-Console.WriteLine("Введите количество строк двумерного массива m: ");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Введите количество строк двумерного массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов двумерного массива n: ");
+Console.WriteLine("Введите количество столбцов двумерного массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] arr = new int[m, n];
-void OrderArr(int[,] arr)
+Create_arr_random(arr);
+Write_arr(arr);
+int minSumLine = 0;
+int sumLine = SumLineElements(arr, 0);
+for (int i = 1; i < arr.GetLength(0); i++)
 {
-  for (int i = 0; i < arr.GetLength(0); i++)
+  int tempSumLine = SumLineElements(arr, i);
+  if (sumLine > tempSumLine)
   {
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-      for (int k = 0; k < arr.GetLength(1) - 1; k++)
-      {
-        if (arr[i, k] < arr[i, k + 1])
-        {
-          int temp = arr[i, k + 1];
-          arr[i, k + 1] = arr[i, k];
-          arr[i, k] = temp;
-        }
-      }
-    }
+    sumLine = tempSumLine;
+    minSumLine = i;
   }
 }
-void Randarr(int[,] arr)
+Console.WriteLine($"В строке {minSumLine+1} - наименьшая сумма элементов");
+int SumLineElements(int[,] arr, int i)
+{
+  int sumLine = arr[i,0];
+  for (int j = 1; j < arr.GetLength(1); j++)
+  {
+    sumLine += arr[i,j];
+  }
+  return sumLine;
+}
+void Create_arr_random(int[,] arr)
 {
   for (int i = 0; i < arr.GetLength(0); i++)
   {
     for (int j = 0; j < arr.GetLength(1); j++) arr[i, j] = new Random().Next(100);
   }
 }
-void Writearr(int[,] arr)
+void Write_arr (int[,] arr)
 {
   for (int i = 0; i < arr.GetLength(0); i++)
   {
-    for (int j = 0; j < arr.GetLength(1); j++) Console.Write(arr[i, j] + " "); Console.WriteLine();
+    for (int j = 0; j < arr.GetLength(1); j++) Console.Write(arr[i,j] + " ");
+    Console.WriteLine();
   }
 }
-Console.WriteLine($"\nЗаданный массив:");
-Randarr(arr);
-Writearr(arr);
-Console.WriteLine($"\nВ итоге получается вот такой массив:");
-OrderArr(arr);
-Writearr(arr);
