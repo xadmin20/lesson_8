081d23837033af63155cc5c6abe2c91f5f894a26
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-15 11:33:30 +0300

    Задача №58

--- Program.cs
+++ Program.cs
@@ -1,44 +1,40 @@
-﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-Console.WriteLine("Введите количество строк двумерного массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов двумерного массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] arr = new int[m, n];
-Create_arr_random(arr);
-Write_arr(arr);
-int minSumLine = 0;
-int sumLine = SumLineElements(arr, 0);
-for (int i = 1; i < arr.GetLength(0); i++)
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.WriteLine("Введите количество строк 1-ой матрицы: "); int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов 1-ой и количество строк 2-ой: "); int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов 2-ой матрицы: "); int p = Convert.ToInt32(Console.ReadLine());
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix); Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix); Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+int[,] arrMatrix = new int[m,p];
+MultiplyMatrix(firstMartrix, secomdMartrix, arrMatrix); Console.WriteLine($"Произведение матриц:");
+WriteArray(arrMatrix);
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-  int tempSumLine = SumLineElements(arr, i);
-  if (sumLine > tempSumLine)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
   {
-    sumLine = tempSumLine;
-    minSumLine = i;
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++) sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      resultMatrix[i,j] = sum;
+    }
   }
 }
-Console.WriteLine($"В строке {minSumLine+1} - наименьшая сумма элементов");
-int SumLineElements(int[,] arr, int i)
+void CreateArray(int[,] array)
 {
-  int sumLine = arr[i,0];
-  for (int j = 1; j < arr.GetLength(1); j++)
+  for (int i = 0; i < array.GetLength(0); i++)
   {
-    sumLine += arr[i,j];
-  }
-  return sumLine;
-}
-void Create_arr_random(int[,] arr)
-{
-  for (int i = 0; i < arr.GetLength(0); i++)
-  {
-    for (int j = 0; j < arr.GetLength(1); j++) arr[i, j] = new Random().Next(100);
+    for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(10);
   }
 }
-void Write_arr (int[,] arr)
+void WriteArray (int[,] array)
 {
-  for (int i = 0; i < arr.GetLength(0); i++)
+  for (int i = 0; i < array.GetLength(0); i++)
   {
-    for (int j = 0; j < arr.GetLength(1); j++) Console.Write(arr[i,j] + " ");
+    for (int j = 0; j < array.GetLength(1); j++) Console.Write(array[i,j] + " ");
     Console.WriteLine();
   }
 }
