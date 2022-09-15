ae6b8861a2e66f530ff3415a181b9ed3c6cf3210
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-15 12:56:40 +0300

    Задача №62

--- Program.cs
+++ Program.cs
@@ -1,46 +1,34 @@
-﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-Console.WriteLine("Введите размер массива x: "); int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите размер массива y: "); int y = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите размер массива z: "); int z = Convert.ToInt32(Console.ReadLine());
-int[,,] arr = new int[x, y, z];
-CreateArray(arr);
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+int n = 4;
+int[,] arr = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= arr.GetLength(0) * arr.GetLength(1))
+{
+  arr[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < arr.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= arr.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > arr.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
 WriteArray(arr);
-void WriteArray (int[,,] arr)
-{
-  for (int i = 0; i < arr.GetLength(0); i++)
-  {
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-      for (int k = 0; k < arr.GetLength(2); k++) Console.Write( $"{arr[i,j,k]}({j}, {k}, {i}) ");
-      Console.WriteLine();
-    }
-  }
-}
-void CreateArray(int[,,] arr)
-{
-  int[] temp = new int[arr.GetLength(0) * arr.GetLength(1) * arr.GetLength(2)];
-  int  number;
-  for (int i = 0; i < temp.GetLength(0); i++)
-  {
-    temp[i] = new Random().Next(10, 100); number = temp[i];
-    if (i >= 1)
-    {
-      for (int j = 0; j < i; j++)
-      {
-        while (temp[i] == temp[j])
-        {
-          temp[i] = new Random().Next(10, 100); j = 0; number = temp[i];
-        }
-          number = temp[i];
-      }
-    }
-  }
-  int count = 0; 
-  for (int x = 0; x < arr.GetLength(0); x++)
-  {
-    for (int y = 0; y < arr.GetLength(1); y++)
-    {
-      for (int z = 0; z < arr.GetLength(2); z++) arr[x, y, z] = temp[count]; count++;
-    }
-  }
-}
